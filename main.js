6fbfcdd728b3f46d22adccea71d562111248d0bf
Charles Morris <percworld@gmail.com>
2020-12-19 14:57:45 -0700

    user input display

--- main.js
+++ main.js
@@ -49,18 +49,18 @@ var desserts = [
   "Eclairs"
 ]
 
+var userType;
 
 var cookPotIcon = document.querySelector('.pot');
 var cookButton = document.querySelector('.cook-button');
-
 var clearButton = document.querySelector('.clear-button');
-var line2 = document.querySelector('.line2');
+var displayed = document.querySelector('.line2');
 var recommend = document.querySelector('.recommend');
 
 
 // Event Listeners
-cookButton.addEventListener('click', getData)
-clearButton.addEventListener('click', clear)
+cookButton.addEventListener('click', getData);
+clearButton.addEventListener('click', clear);
 
 //Event Handlers
 function getRandomIndex(array) {
@@ -81,56 +81,54 @@ function getData() {
   showMeal(choice);
 };
 
-
-
-
-
 function showMeal(choice) {             //Let's Cook button handler
   cookPotIcon.classList.add('hidden');
   clearButton.classList.remove('hidden');
   recommend.classList.remove('hidden');
   cookButton.classList.add('hidden');
-  line2.innerText = `${choice}!`;
-}
+  displayed.innerText = `${choice}!`;
+};
 
 function clear() {
   cookPotIcon.classList.remove('hidden');
   clearButton.classList.add('hidden');
   recommend.classList.add('hidden');
   cookButton.classList.remove('hidden');
-}
+};
 var recipeButton = document.querySelector('.recipe-button');
 recipeButton.addEventListener('click', userRecipe);
 var recipeBar = document.querySelector('.add-recipe-bar');
 var makeNew = document.querySelector('.add-new-button');
 var type = document.querySelector('.user-type');
 var dish = document.querySelector('.user-dish');
-makeNew.addEventListener('click', addRecipe)
+makeNew.addEventListener('click', addRecipe);
 
 function userRecipe() {
   recipeButton.classList.add('hidden');
   recipeBar.classList.remove('hidden');
-}
+};
+
 
 function addRecipe() {  //type of dish shows result, calls saving to array
-  var userType = 'side';
-  var userDish = 'Lasagna';
+  // var userType = type;
+  // var userDish = dish;
   recipeButton.classList.remove('hidden');
   recipeBar.classList.add('hidden');
-  if (type != undefined && typeof(type)==='string') {
-    console.log('which');
 
-  }
-
-  saveMeal(userType, userDish);
-
-
-
-  showMeal(userDish);
+  if ((typeof 'type') === 'string' && (typeof 'dish') === 'string') {  //Enough validation?
+    userType = type.innerText;
+    choice = dish.innerText;
+    saveMeal(userType, choice);
+  };
+  showMeal(choice);
 };
 
 function saveMeal(type, choice) {   // save the dish into it's proper array
   if (type.toLowerCase === 'side' ) {
-    console.log('ok')
-  }
-}
+    sideDishes.push(choice);
+  } else if (type.toLowerCase === 'main') {
+    mainDishes.push(choice);
+  } else if (type.toLowerCase === 'dessert') {
+    desserts.push(choice);
+  };
+};
