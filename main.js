43e83819dde1508f2e6682e6dc01c8a8dd2fccd6
Charles Morris <percworld@gmail.com>
2020-12-31 01:06:34 -0700

    refactor and css update

--- main.js
+++ main.js
@@ -21,7 +21,7 @@ var favoriteRecipes = document.querySelector('.favorite-recipes');
 var backButton = document.querySelector('.back-button');
 
 // Event Listeners
-cookButton.addEventListener('click', getData);
+cookButton.addEventListener('click', chooseRandomRecipe);
 clearButton.addEventListener('click', clear);
 favoriteButton.addEventListener('click', addFavorite);
 recipeButton.addEventListener('click', userRecipe);
@@ -30,102 +30,96 @@ favoritesViewButton.addEventListener('click', viewFavorites);
 backButton.addEventListener('click', goBack);
 
 //Event Handlers
+function hide(element) {
+  element.classList.add('hidden');
+};
+
+function show(element) {
+  element.classList.remove('hidden');
+};
+
 function getRandomIndex(array) {
   return Math.floor(Math.random() * array.length);
 };
 
-function getData() {
-  var index;
+function chooseRandomRecipe() {
   displayed.classList.add('line2');
   if (document.getElementById("meal").checked === true) {
     makeMeal();
-    return;
-  }
-  if (document.getElementById("side").checked === true) {
-    index = getRandomIndex(sideDishes);
-    choice = sideDishes[index];
-    sideDishes.splice(index, 1);
+  } else if (document.getElementById("side").checked === true) {
+    choice = chooseDish(sideDishes);
   } else if (document.getElementById("main").checked === true) {
-      index = getRandomIndex(mainDishes);
-      choice = mainDishes[index];
-      mainDishes.splice(index, 1);
-    } else if (document.getElementById("dessert").checked === true) {
-      index = getRandomIndex(desserts);
-      choice = desserts[index];
-      desserts.splice(index, 1);
-    } else choice = "Oops, you forgot to make a selection";
-        //else if (document.getElementById("meal").checked === true) {
-        //choice = "Update for a full meal coming soon";
-        //  } else choice = "Cloudy with a chance of Meatballs";
+      choice = chooseDish(mainDishes);
+  } else if (document.getElementById("dessert").checked === true) {
+      choice = chooseDish(desserts);
+  } else choice = "Oops, you forgot to make a selection";
   showMeal(choice);
+};
+
+function chooseDish(type) {
+  var index = getRandomIndex(type);
+  choice = type[index];
+  type.splice(index, 1);
+  return choice;
 };
 
 function makeMeal() {
-  var meal = "";
-  choice = "";
-  index = getRandomIndex(mainDishes);
-  choice += mainDishes[index];
-  choice += " with a side of "
-  mainDishes.splice(index, 1);
-  index = getRandomIndex(sideDishes);
-  choice += sideDishes[index];
-  choice += " and ";
-  sideDishes.splice(index, 1);
-  index = getRandomIndex(desserts);
-  choice += desserts[index];
-  desserts.splice(index, 1);
-  choice += " for dessert!"
+  choice = `${chooseDish(mainDishes)} with a side of ${chooseDish(sideDishes)} and ${chooseDish(desserts)} for dessert!`;
   displayed.classList.add('line2-meal');
-  showMeal(choice);
-}
+};
 
-function showMeal(choice) {             //Let's Cook button handler
+function showMeal(choice) {
   hideUserRecipe();
-  cookPotIcon.classList.add('hidden');
-  clearButton.classList.remove('hidden');
-  recommend.classList.remove('hidden');
-  cookButton.classList.add('hidden');
+  hide(cookPotIcon);
+  hide(cookButton);
+  show(clearButton);
+  show(recommend);
+  show(favoriteButton);
+  show(favoritesViewButton);
   displayed.innerText = `${choice}!`;
-  favoriteButton.classList.remove('hidden');
 };
 
 function clear() {
-  cookPotIcon.classList.remove('hidden');
-  clearButton.classList.add('hidden');
-  recommend.classList.add('hidden');
-  cookButton.classList.remove('hidden');
-  favoriteButton.classList.add('hidden');
+  hide(favoriteRecipes);
+  hide(clearButton);
+  hide(recommend);
+  hide(favoriteButton);
+  show(cookPotIcon);
+  show(cookButton);
 };
 
 function userRecipe() {
-  recipeButton.classList.add('hidden');
-  recipeBar.classList.remove('hidden');
+  goBack();
+  hide(recipeButton);
+  show(recipeBar);
+  hide(favoritesViewButton);
 };
 
 function hideUserRecipe() {
-  recipeButton.classList.remove('hidden');
-  recipeBar.classList.add('hidden');
-}
-
-function addRecipe() {
-  recipeButton.classList.remove('hidden');
-  recipeBar.classList.add('hidden');
-  favoritesViewButton.classList.remove('hidden');
-  if ((typeof 'type') === 'string' && (typeof 'dish') === 'string') {  //Enough validation?
-    userType = type.innerText;
-    choice = dish.innerText;
-    saveMeal(userType, choice);
-  };
-  userType = type.value;
-  choice = dish.value;
-  saveMeal(userType, choice);
-  showMeal(choice);
+  show(recipeButton);
+  hide(recipeBar);
 };
 
-function saveMeal(type, choice) {   // save the dish into it's proper array
+function addRecipe() {
+  hideUserRecipe();
+  show(favoritesViewButton);
+  userType = type.value;
+  choice = dish.value;
+  if (typeof userType == 'string' && typeof choice == 'string') {
+    saveMeal(userType, choice);
+    showMeal(choice);
+  } else return;
+  clearFields();
+};
+
+function clearFields() {
+  dish.value = "";
+  type.value = "";
+};
+
+function saveMeal(type, choice) {
   if (type.toLowerCase === 'side' ) {
     sideDishes.push(choice);
-    console.log(choice);
   } else if (type.toLowerCase === 'main' || type.toLowerCase === 'main dish') {
     mainDishes.push(choice);
   } else if (type.toLowerCase === 'dessert') {
@@ -136,25 +130,30 @@ function saveMeal(type, choice) {   // save the dish into it's proper array
 };
 
 function addFavorite() {
-  favorites.unshift(choice);
+  if (choice != "Oops, you forgot to make a selection" && !favorites.includes(choice)) {
+    favorites.unshift("   " + choice);
+  };
 };
 
 function viewFavorites() {
   hideUserRecipe();
-  choiceView.classList.add('hidden');
-  favoritesView.classList.remove('hidden');
-  favoritesViewButton.classList.add('hidden');
-  favoritesImage.classList.remove('hidden');
-  favoriteRecipes.classList.remove('hidden');
-  backButton.classList.remove('hidden');
+  hide(choiceView);
+  hide(favoritesViewButton);
+  show(favoritesView);
+  show(favoritesImage);
+  show(favoriteRecipes);
+  show(backButton);
   favoriteRecipes.innerHTML = favorites;
-}
+};
 
 function goBack() {
-  backButton.classList.add('hidden');
-  choiceView.classList.remove('hidden');
-  favoritesView.classList.add('hidden');
-  favoritesViewButton.classList.remove('hidden');
-  favoritesImage.classList.add('hidden');
-  favoriteRecipes.classList.add('hidden');
-}
+  hide(backButton);
+  hide(favoritesImage);
+  hide(favoritesView);
+  hide(favoriteRecipes);
+  show(choiceView);
+  show(favoritesViewButton);
+  clear();
+};
+
+clear();
